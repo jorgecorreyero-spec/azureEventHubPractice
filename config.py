fa616bb0b5fa3eca1ade67017925705adcd622b4
Jorge Correyero <jorge.correyero@bluetab.net>
2025-09-24 13:53:03 +0200

    Removed eventhub name global var

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
+# URL for our data source
 API_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
 # Event Hub connection config
 EVENTHUB_CONNECTION_STR = ""
-EVENTHUB_NAME = "earthquakes"
 
 # Time waited for new request to the API
 FETCH_INTERVAL = 60
